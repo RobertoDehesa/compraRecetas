a9584e6e81f9fcdb3182454172e345c690def0c6
Roberto Dehesa <RobertoDG@macbook-pro-de-roberto-dehesa.local>
2014-12-27 17:27:24 +0100

    Initial Commit

---
+++ recetas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recetas
+//
+//  Created by Roberto Dehesa  on 27/12/14.
+//  Copyright (c) 2014 Roberto Dehesa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
